d8178ae55f9dae5d15b1ac148825bbe78bea8d0b
wyeth <wyeth_irish_mendez@gmail.com>
2026-08-15 22:32:34 +0800

    Add GT3-PT1 project code

--- node_modules/react-router/dist/development/lib/types/future.d.ts
+++
@@ -1,9 +0,0 @@
-
-//#region lib/types/future.d.ts
-/**
- * An augmentable interface users can modify in their app-code to opt into
- * future-flag-specific types
- */
-interface Future {}
-//#endregion
-export { Future };
--- node_modules/react-router/dist/production/lib/server-runtime/dev.d.ts
+++
@@ -1,9 +0,0 @@
-
-//#region lib/server-runtime/dev.d.ts
-type DevServerHooks = {
-  getCriticalCss?: (pathname: string) => Promise<string | undefined>;
-  processRequestError?: (error: unknown) => void;
-};
-declare function setDevServerHooks(devServerHooks: DevServerHooks): void;
-//#endregion
-export { setDevServerHooks };
